155ce93f463f601a384b98ed0bcce47d88fdd68c
ZkHaider <hkhan@oglethorpe.edu>
2015-01-24 16:03:41 -0500

    Added Material Tabs

---
+++ mobile/src/main/java/com/zkhaider/red_code/ui/ProductActivity.java
@@ -0,0 +1,13 @@
+package com.zkhaider.red_code.ui;
+
+import android.app.Activity;
+
+/**
+ * Created by Haider on 1/24/2015.
+ */
+public class ProductActivity extends Activity {
+
+
+
+
+}
--- mobile/src/main/java/com/zkhaider/red_code/ui/ProductDetailsActivity.java
+++
@@ -1,7 +0,0 @@
-package com.zkhaider.red_code.ui;
-
-/**
- * Created by Haider on 1/24/2015.
- */
-public class ProductDetailsActivity {
-}
