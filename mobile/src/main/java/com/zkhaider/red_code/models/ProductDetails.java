2932e3700d92fdd55c645ae9d95bd399bd419114
KevinColemanInc <kevin.coleman@sparkstart.io>
2015-01-24 15:50:50 -0500

    lasjkdf

--- mobile/src/main/java/com/zkhaider/red_code/models/ProductDetails.java
+++ mobile/src/main/java/com/zkhaider/red_code/models/ProductDetails.java
@@ -4,4 +4,15 @@ package com.zkhaider.red_code.models;
  * Created by kcoleman on 1/24/15.
  */
 public class ProductDetails {
+    String mName;
+    String mPrice;
+    String mRating;
+    String
+    // name
+    // description
+    // rating
+    // id
+    // price
+    // image_url;
+
 }
